be63ec5bf4369d187447804c81265375eebbf9d6
jeeyulee <jeeyu-lee@naver.com>
2022-11-20 23:03:09 +0900

    cors 경로 수정

--- backend/src/main/java/com/jeeyulee/mongddang/common/config/WebMvcConfig.java
+++ backend/src/main/java/com/jeeyulee/mongddang/common/config/WebMvcConfig.java
@@ -34,7 +34,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedHeaders("*")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowCredentials(true);
