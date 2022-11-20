144269c238b7ab6a855bee6614d497ffa4cd314d
jeeyulee <jeeyu-lee@naver.com>
2022-11-20 23:16:13 +0900

    cors 설정 변경

--- backend/src/main/java/com/jeeyulee/mongddang/common/config/WebMvcConfig.java
+++ backend/src/main/java/com/jeeyulee/mongddang/common/config/WebMvcConfig.java
@@ -34,7 +34,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedHeaders("*")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowCredentials(true);
